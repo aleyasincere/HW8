88e27163ae1194d7bc27f84602da123f43de7b5a
aleyasincere <119109941+aleyasincere@users.noreply.github.com>
2024-06-04 19:32:32 +0600

    code refactoring

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,12 @@
-// import React from 'react'
-// import ModalWindow from '../ModalWindow/ModalWindow'
-//
-// const About = () => {
-//     return (
-//         <div>
-//             <ModalWindow/>
-//         </div>
-//     )
-// }
-//
-// export default About;
+import React from 'react'
+import ModalWindow from '../ModalWindow/ModalWindow'
+
+const About = () => {
+    return (
+        <div>
+            <ModalWindow/>
+        </div>
+    )
+}
+
+export default About;
